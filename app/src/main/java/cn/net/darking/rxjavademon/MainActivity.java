de69f4392ed14478e0423651195114720254221f
LEMON521 <13937260956@yeah.net>
2018-01-16 18:35:31 +0800

    test onNext() and onComplete

--- app/src/main/java/cn/net/darking/rxjavademon/MainActivity.java
+++ app/src/main/java/cn/net/darking/rxjavademon/MainActivity.java
@@ -34,21 +34,40 @@ public class MainActivity extends AppCompatActivity {
                 emitter.onNext(2);
                 emitter.onNext(3);
                 emitter.onNext(4);
+                /**
+                 * 在onComplete()之-前-,emitter为null
+                 * 在onComplete()之-后-,emitter为DISPOSED
+                 */
                 emitter.onComplete();//Complete:完成,结束
+                emitter.onNext(6);//这里就不执行了
+                emitter.onNext(7);//这里就不执行了
+                emitter.onComplete();//Complete:完成,结束
+                emitter.onNext(6);//这里就不执行了
+                emitter.onNext(7);//这里就不执行了
 
             }
         });
 
         //观察者-水管下游-处理
         Observer<Integer> observer = new Observer<Integer>() {
+
+            /**
+             * 最先执行
+             * @param d
+             */
             @Override
             public void onSubscribe(@NonNull Disposable d) {
-                Log.d(TAG, "subscribe");
+                Log.d(TAG, "subscribe" + d);
             }
 
             @Override
             public void onNext(@NonNull Integer integer) {
-                Log.d(TAG, "next==" + integer);
+                if (integer % 2 == 0) {
+                    Log.d(TAG, "next==" + integer + "::" + (integer / 2));
+                } else {
+                    this.onError(new Throwable());
+                }
+
             }
 
             @Override
